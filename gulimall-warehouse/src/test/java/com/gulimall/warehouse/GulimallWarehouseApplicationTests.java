121ef51279b8f2fa1f388568eb7462f2ecac806c
Xiaotian <cblxxlx@gmail.com>
2020-12-23 02:39:09 +1100

    项目初始结构创建

---
+++ gulimall-warehouse/src/test/java/com/gulimall/warehouse/GulimallWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gulimall.warehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallWarehouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
